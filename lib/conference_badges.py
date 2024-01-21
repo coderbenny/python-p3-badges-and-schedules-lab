ea97182928c62252027a57c8b032847660917650
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-01-21 19:05:30 +0300

    Completed assignment

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
+print(badge_maker("Benny"))
 
 def batch_badge_creator(names):
-    return None
+    badges = ['Hello, my name is ' + item + '.' for item in names]
+    return badges        
+
+print(batch_badge_creator(["Arel","Carol"]))
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, len(names) + 1)
+    return [f"Hello, {name}! You'll be assigned to room {room}!" for room, name in enumerate(names, start = 1)]
+
+print(assign_rooms(["Alice", "Bob", "Charlie", "Diana", "Edward", "Fiona", "George"]))
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignment = assign_rooms(names)
+
+    for item in badges:
+        print(item)
+
+    for room in room_assignment:
+        print(room)
+
+printer(["Arel", "Carol"])
